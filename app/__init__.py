56326512b85d411cce4ea9c9c5e4293b5cf35ced
Muniba <mbashirkimes@gmail.com>
2023-07-24 19:30:14 -0400

    Imports and configures CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/toy_library_development'
