adff91f3b3776be199aab9dcf84864329466e761
Muniba <mbashirkimes@gmail.com>
2023-07-28 10:56:49 -0400

    fixing db info

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(test_config=None):
     CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/toy_library_development'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
 
 
