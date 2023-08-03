44ffeffabc3fc5e65df4769b8c18e40612bee1e3
Muniba <mbashirkimes@gmail.com>
2023-08-03 13:19:51 -0400

    env file fixed

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from dotenv import load_dotenv 
 import os
 
 db = SQLAlchemy()
